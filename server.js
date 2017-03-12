e4d2875c3a3a893032361107a3092058fbe91dd5
krshna90 <krshnapatel9034@hotmail.com>
2017-03-12 12:06:26 +0530

    prep repo for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require("express");
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+//Redirect request
+app.use(function(req, res, next){
+  if(req.header["x-forwarded-proto"] === "http"){
+    next();
+  }else{
+    res.redirect("http://" + req.hostname + req.url)
+  }
+});
 app.use(express.static("public"));
-app.listen(3000,function(){
+app.listen(PORT, function(){
   console.log("Server up and running for New React App at port:3000");
 });
